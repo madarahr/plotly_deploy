88f480eb8a7720d34b35cdd63ee384dc544e944d
Rahul <madarahr40@gmail.com>
2020-08-16 21:44:08 -0500

    adding plotly files to repo

--- plots.js
+++ plots.js
@@ -1,38 +1,37 @@
-function init() {
-    var selector = d3.select("#selDataset");
-  
-    d3.json("samples.json").then((data) => {
-      console.log(data);
-      var sampleNames = data.names;
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  })}
-  
-  init();
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-  function optionChanged(newSample) {
-    buildMetadata(newSample);
-    buildCharts(newSample);
-  }
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-  function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
-      var PANEL = d3.select("#sample-metadata");
-  
-      PANEL.html("");
-      PANEL.append("h6").text('ID: '+ result.id);
-      PANEL.append("h6").text('ETHNICITY: ' + result.ethnicity);
-      PANEL.append("h6").text('GENDER: '+ result.gender);
-      PANEL.append("h6").text('AGE: ' + result.age);
-      PANEL.append("h6").text('LOCATION: ' + result.location);
-      PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
-      PANEL.append("h6").text('WFREQ: ' + result.wfreq);
-    });
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
+
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
   }
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
